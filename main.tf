79f5d563f5beb06f10bbd5341eabfb0debe8411f
Poonam <povnamk@gmail.com>
2023-09-10 11:51:51 +0000

    Added ec2

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+  }
+
+
+
+}
+#Configure aws provider
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+#Create EC2 instance
+resource "aws_instance" "myec2"{
+    ami = "ami-0bcf9688fa5e01340"
+    instance_type = "t2.micro"
+}
